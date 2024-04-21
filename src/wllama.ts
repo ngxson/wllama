7fbfa37b06a32fa4927e1af1010956eeb5e2e364
ngxson <thichthat@gmail.com>
2024-04-21 15:40:37 +0200

    fix SamplingConfig

--- src/wllama.ts
+++ src/wllama.ts
@@ -52,12 +52,16 @@ export interface SamplingConfig {
   penalty_repeat?: number,
   penalty_freq?: number,
   penalty_present?: number,
+  penalize_nl?: boolean,
+  dynatemp_range?: number,
+  dynatemp_exponent?: number,
   grammar?: string,
   n_prev?: number,
   n_probs?: number,
   min_p?: number,
   tfs_z?: number,
   typical_p?: number,
+  logit_bias?: { token: number, bias: number }[],
 };
 
 export class Wllama {
@@ -255,28 +259,7 @@ export class Wllama {
    * @param config 
    * @param pastTokens In case re-initializing the ctx_sampling, you can re-import past tokens into the new context
    */
-  async samplingInit(config: {
-    // See sampling.h for more details
-    mirostat?: number, // 0 = disabled, 1 = mirostat, 2 = mirostat 2.0
-    mirostat_tau?: number,
-    temp?: number, // temperature
-    top_p?: number,
-    top_k?: number,
-    penalty_last_n?: number,
-    penalty_repeat?: number,
-    penalty_freq?: number,
-    penalty_present?: number,
-    penalize_nl?: boolean,
-    dynatemp_range?: number,
-    dynatemp_exponent?: number,
-    grammar?: string,
-    n_prev?: number,
-    n_probs?: number,
-    min_p?: number,
-    tfs_z?: number,
-    typical_p?: number,
-    logit_bias?: { token: number, bias: number }[],
-  }, pastTokens: number[] = []): Promise<void> {
+  async samplingInit(config: SamplingConfig, pastTokens: number[] = []): Promise<void> {
     this.samplingConfig = config;
     const result = await this.proxy.wllamaAction('sampling_init', {
       ...config,
