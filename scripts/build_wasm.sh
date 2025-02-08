a227c1e2a7f3bd0de8bbc504ddde8189b862ce96
Xuan-Son Nguyen <thichthat@gmail.com>
2025-02-08 22:55:33 +0100

    temporary apply that viral x2 speedup PR (#156)

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $CURRENT_PATH
@@ -8,4 +8,13 @@ cd $CURRENT_PATH
 export D_UID=$UID
 export D_GID=$GID
 
+# temporary apply that viral x2 speedup PR lmao
+wget https://patch-diff.githubusercontent.com/raw/ggerganov/llama.cpp/pull/11453.patch -O ../tmp.patch
+cd ../llama.cpp
+git apply ../tmp.patch
+cd $CURRENT_PATH
+
 docker compose up llamacpp-wasm-builder --exit-code-from llamacpp-wasm-builder
+
+cd ../llama.cpp
+git reset --hard HEAD
