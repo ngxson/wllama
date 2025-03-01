378b544cb100242e71ee1fd108423b8c991224a4
Xuan-Son Nguyen <thichthat@gmail.com>
2025-03-01 20:58:20 +0100

    update emsdk to 4.0.3 (#158)
    * update emsdk to 4.0.3
    
    * fix build
    
    * v2.2.1
    
    * Revert "v2.2.1"
    
    This reverts commit bf902dae705da124af12ba72f98f61d23ab9b71d.

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -2,19 +2,18 @@
 
 #set -e
 
+export EMSDK_IMAGE_TAG="4.0.3"
+
 CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $CURRENT_PATH
 
 export D_UID=$UID
 export D_GID=$GID
 
-# temporary apply that viral x2 speedup PR lmao
-wget https://patch-diff.githubusercontent.com/raw/ggerganov/llama.cpp/pull/11453.patch -O ../tmp.patch
-cd ../llama.cpp
-git apply ../tmp.patch
-cd $CURRENT_PATH
+if [[ $(uname -m) == "arm64" ]]; then
+  echo "Running on ARM64 processor"
+  export DOCKER_DEFAULT_PLATFORM="linux/arm64"
+  export EMSDK_IMAGE_TAG="${EMSDK_IMAGE_TAG}-arm64"
+fi
 
 docker compose up llamacpp-wasm-builder --exit-code-from llamacpp-wasm-builder
-
-cd ../llama.cpp
-git reset --hard HEAD
