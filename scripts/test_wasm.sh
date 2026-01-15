59c30f3fb799cb4c673d845f4310a3d50d9092f8
Antoine Viallon <antoine@lesviallon.fr>
2026-01-15 13:35:59 +0100

    tests: add script to easily run llama.cpp's ctest suite

---
+++ scripts/test_wasm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+export EMSDK_IMAGE_TAG="4.0.23"
+
+CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $CURRENT_PATH
+
+export D_UID=$UID
+export D_GID=$GID
+
+if [[ $(uname -m) == "arm64" ]]; then
+  echo "Running on ARM64 processor"
+  export DOCKER_DEFAULT_PLATFORM="linux/arm64"
+  export EMSDK_IMAGE_TAG="${EMSDK_IMAGE_TAG}-arm64"
+fi
+
+docker compose up --build --no-log-prefix llamacpp-wasm-tests --abort-on-container-exit --exit-code-from llamacpp-wasm-tests
